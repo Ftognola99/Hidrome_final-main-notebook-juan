68ccfafe06f9208621d390268f5017b01fa4b49f
Ftognola99 <98433644+Ftognola99@users.noreply.github.com>
2022-11-28 17:46:21 -0300

    .

--- Slider v2/script.js
+++ Slider v2/script.js
@@ -1,8 +1,14 @@
+// sorry for the spaghetti code and redundant variables, i wasn't exactly a good coder back then
+
 const cols = 3;
 const main = document.getElementById('main');
 let parts = [];
 
-let images url
+let images = [
+  "https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80",
+  "https://images.unsplash.com/photo-1544198365-f5d60b6d8190?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80",
+  "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2700&q=80"
+];
 let current = 0;
 let playing = false;
 
