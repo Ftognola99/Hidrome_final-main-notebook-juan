9ad80078892a997c70006fed87d8e00eec575ce9
Ftognola99 <98433644+Ftognola99@users.noreply.github.com>
2022-12-12 13:39:24 -0300

    .

---
+++ urbanui-login-template-free-1-7433a5e503a6/main.js
@@ -0,0 +1,10 @@
+const signupForm = document.querySelector('#signup-form')
+
+signupForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const signupEmail = document.queryselector('#signup-form');
+    const signupPassword = document.queryselector('#signup-password')
+
+        console.log(signupEmail, signupPassword)
+})
